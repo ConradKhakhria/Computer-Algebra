3236a6308c631b49dab7fff382df249957361d53
Conrad Khakhria <67520159+ConradKhakhria@users.noreply.github.com>
2020-09-29 14:19:26 +0100

    Update rearrange.rkt
    Re-included the actual rearrange function
    
    There is a known contract violation occurring in rearrange but currently I cannot for the life of me work out why. More testing shall follow..

--- rearrange.rkt
+++ rearrange.rkt
@@ -11,11 +11,13 @@
     ((equal? '= sym) '=)))
 
 (define (exclude-index xs i current-i)
+  "exclude the element at index 'i' from the list xs"
   (if (< current-i i)
     (cons (car xs) (exclude-index (cdr xs) i (+ current-i 1)))
     (cdr xs)))
 
 (define (get-indices expr i sym)
+  "gets the paths through 'expr' to sym"
   (cond
     [(null? expr)
       #f]
@@ -28,3 +30,23 @@
       (list i)]
     [#t
       (get-indices (cdr expr) (+ i 1) sym)]))
+      
+(define (rearrange equation var)
+  "rearrange 'equation' for variable 'var'"
+  (define (rearr-iter expr path other-side)
+    (if (pair? expr)
+      (rearr-iter
+        (list-ref expr (car path))
+        (cdr path)
+        (cons
+          `,(opposite-sym (car expr))
+          (cons
+            `,other-side
+            `,(exclude-index (cdr expr) (car path) 0))))
+      other-side))
+  (let ([path (get-indices equation 0 var)])
+    (rearr-iter
+      (list-ref equation (car path))
+      (cdr path)
+      (exclude-index (cdr equation) (car path) 0))))
+     
